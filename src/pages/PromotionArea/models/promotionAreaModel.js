9563509cbe56a2ababd8c101af2892bac1065e65
Una <una_bella@qq.com>
2019-03-15 11:54:10 +0800

    临期促销上线403处理
    about #12

--- src/pages/PromotionArea/models/promotionAreaModel.js
+++ src/pages/PromotionArea/models/promotionAreaModel.js
@@ -1,7 +1,7 @@
 import { queryRule, removeRule, addRule, updateRule } from '@/services/api';
 
 export default {
-  namespace: 'JapanPavilion',
+  namespace: 'promotionAreaModel',
 
   state: {
     data: {
@@ -10,46 +10,7 @@ export default {
     },
   },
 
-  effects: {
-    *fetch({ payload }, { call, put }) {
-      const response = yield call(queryRule, payload);
-      yield put({
-        type: 'save',
-        payload: response,
-      });
-    },
-    *add({ payload, callback }, { call, put }) {
-      const response = yield call(addRule, payload);
-      yield put({
-        type: 'save',
-        payload: response,
-      });
-      if (callback) callback();
-    },
-    *remove({ payload, callback }, { call, put }) {
-      const response = yield call(removeRule, payload);
-      yield put({
-        type: 'save',
-        payload: response,
-      });
-      if (callback) callback();
-    },
-    *update({ payload, callback }, { call, put }) {
-      const response = yield call(updateRule, payload);
-      yield put({
-        type: 'save',
-        payload: response,
-      });
-      if (callback) callback();
-    },
-  },
+  effects: {},
 
-  reducers: {
-    save(state, action) {
-      return {
-        ...state,
-        data: action.payload,
-      };
-    },
-  },
+  reducers: {},
 };
